2d0212d14e185bb798e8640b8621866a1f22db6f
George Joseph <thegeorgejoseph@gmail.com>
2021-04-25 15:09:48 +0530

    added textfield, connected it to useState and also customised styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import NavBar from './components/NavBar'
-
+import Search from './components/Search'
 function App() {
   return (
     <div className="App">
      <NavBar/>
+     <Search/>
     </div>
   );
 }
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Select from '@material-ui/core/Select';
 
+const useStyles = makeStyles((theme) => ({
+    root: {
+      '& > *': {
+        margin: theme.spacing(1),
+        width: '25ch',
+      },
+    },
+    textfield : {
+        width: '100%',
+    },
+  }));
 const Search = () => {
+    const classes = useStyles();
+    const initialState = {
+        searchText : '',
+        amount : 15,
+        url : 'https://pixabay.com/api',
+        key : process.env.REACT_APP_API_KEY,
+        images : [],
+    }
+    const [state,setState] = useState(initialState);
+    const onTextChange = (e) => {
+    
+        setState(prevState => ({...prevState,searchText:e.target.value}))
+
+    }
+    
     return (
         <div>
-            
+            <TextField className={classes.textfield} id="standard-basic" label="Search For Images" value={state.searchText} onChange={onTextChange}  />
         </div>
     )
 }
